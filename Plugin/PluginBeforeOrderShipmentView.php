59fc119d01711b4b161006047d604a365575c219
Tony Cheetham <Tony@Oddsphere.com>
2017-10-08 23:11:22 +0100

    Tried new format

---
+++ Plugin/PluginBeforeOrderShipmentView.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by OddWare.
+ * User: tonycheetham
+ * Date: 03.07.2017
+ * Time: 10:13
+ */
+
+namespace OddWare\ShipsterConnect\Plugin;
+
+use Magento\Framework\UrlInterface;
+
+class PluginBeforeOrderShipmentView
+{
+    protected $_urlBuilder;
+
+    public function __construct(
+        UrlInterface $urlBuilder
+    )
+    {
+        $this->_urlBuilder = $urlBuilder;
+    }
+
+    public function beforeSetLayout(\Magento\Shipping\Block\Items $subject)
+    {
+		print('This worked!');
+        //$url2 = $this->_urlBuilder->getUrl('shipsterconnect/xml/generate', ['order_id' => $subject->getOrderId()]);
+
+        $message = 'An export file will be generated, please save it to downloads.';
+
+        // add button to order view with, url in button to generate .pvx xml
+        $subject->addButton(
+            'generateshipxml',
+            [
+                'label' => __('Send to Shipster'),
+                'class' => 'xml-button, ship',
+                'onclick' => "confirmSetLocation('{$message}', '{$url2}')"
+            ]
+        );
+
+    }
+}
