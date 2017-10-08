a74587ae899cdee7a28d148466ad63c77642a630
Tony Cheetham <Tony@Oddsphere.com>
2017-10-08 20:24:09 +0100

    Moved files around, to add extra column

---
+++ Ui/Component/Listing/Column/Order/PluginBeforeOrderShipmentView.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by OddWare.
+ * User: tonycheetham
+ * Date: 03.07.2017
+ * Time: 10:13
+ */
+
+namespace OddWare\ShipsterConnect\Ui\Component\Listing\Column\Order;
+
+class PrintAction extends Column {
+
+    public function prepareDataSource(array $dataSource) {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+
+                $viewUrlPath = $this->getData('config/viewUrlPath') ? : '#';
+                $urlEntityParamName = $this->getData('config/urlEntityParamName') ? : 'entity_id';
+
+                $item[$this->getData('name')] = [
+                    'ModuleName' => [
+                        'href' => $this->context->getUrl(
+                                $viewUrlPath, [$urlEntityParamName => $item['entity_id']]
+                        ),
+                        'label' => 'Print',
+                    ]
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+
+}
